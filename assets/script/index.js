c72d112716d8d441b0fa6c03f2e7323a2d2086db
Vincenzo M <vincenzomaiorana@gmail.com>
2022-12-19 20:14:33 +0100

    added stars in feedback

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,1 +1,16 @@
-console.log("Index Started");
+const feedbackSection = document.querySelector("#fieldesetFeedback");
+const stelle = 10;
+for (let i = 0; i < stelle; i++) {
+  const formStelle = ` <p class="feedbackrating">
+          <input id="feedback${i}" type="radio" value="${i}" name="feedback" />
+          <label for="feedback${i}">
+            <img src="./assets/imgs/star.svg" onclick="selectFb(${i})" id="fbImg${i}"alt="${i}" /></label>
+        </p>`;
+
+  feedbackSection.innerHTML += formStelle;
+}
+
+const selectFb = (id) => {
+  document.querySelector(`#fbImg${id}`).classList.toggle("stars_col");
+  console.log("cambio");
+};
