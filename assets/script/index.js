552ea256083a8c0edf7b36527545883d3cf57101
Vincenzo M <vincenzomaiorana@gmail.com>
2022-12-20 09:28:23 +0100

    fix stars selection feedback page

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,4 +1,4 @@
-const feedbackSection = document.querySelector("#fieldesetFeedback");
+const feedbackSection = document.querySelector("#fieldsetFeedback");
 const stelle = 10;
 for (let i = 0; i < stelle; i++) {
   const formStelle = ` <p class="feedbackrating">
@@ -11,6 +11,11 @@ for (let i = 0; i < stelle; i++) {
 }
 
 const selectFb = (id) => {
-  document.querySelector(`#fbImg${id}`).classList.toggle("stars_col");
-  console.log("cambio");
+  const stelleFeed = document.querySelectorAll(".feedbackrating img");
+  for (let i = 0; i < stelleFeed.length; i++) {
+    stelleFeed[i].classList.remove("stars_col");
+  }
+  for (let i = 0; i < id + 1; i++) {
+    stelleFeed[i].classList.add("stars_col");
+  }
 };
