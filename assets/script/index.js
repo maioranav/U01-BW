2c7b8ff34b1a58bc2936fb046fffcc0b2b9e6f9f
Vincenzo M <vincenzomaiorana@gmail.com>
2022-12-20 17:53:35 +0100

    load questions, checkbox required

--- assets/script/index.js
+++ assets/script/index.js
@@ -106,6 +106,61 @@ const startQuestions = () => {
   }
 };
 
+let activeQuestion = -1;
+
+const loadQuestions = (questionsArray) => {
+  const questionsSection = document.querySelector("#questions");
+  for (let i = 0; i < questionsArray.length; i++) {
+    let domande = `<div id="domanda${i}" class="hidden">
+      <h1>${questionsArray[i].question}</h1>
+      <form id="question${i}">
+        <div class="radio-container">`;
+
+    let answers = questionsArray[i].incorrect_answers;
+
+    for (let q = 0; q < answers.length; q++) {
+      domande += `<input id="answer${q}" type="radio" name="options" value="answer${q}" />
+          <label for="answer${q}">${answers[q]}</label>`;
+    }
+
+    domande += `</div>
+        <div id="question-footer">
+          <h3>QUESTION ${i} <span>/${questionsArray.length}</span></h3>
+          <a onclick="nextQuestion()"><ion-icon name="arrow-forward-outline" class="arrow"></ion-icon></a>
+        </div>
+      </form>
+    </div>`;
+
+    questionsSection.innerHTML += domande;
+    document.querySelector("#questions").classList.remove("hidden");
+    nextQuestion();
+  }
+};
+
+const nextQuestion = () => {
+  if (activeQuestion < 0) {
+    activeQuestion += 1;
+    console.log(activeQuestion);
+    document
+      .querySelector(`#domanda${activeQuestion}`)
+      .classList.remove("hidden");
+  } else if (activeQuestion < questions.length && activeQuestion >= 0) {
+    document.querySelector(`#domanda${activeQuestion}`).classList.add("hidden");
+    activeQuestion++;
+    document
+      .querySelector(`#domanda${activeQuestion}`)
+      .classList.remove("hidden");
+  } else {
+    document.querySelector(`#domanda${activeQuestion}`).classList.add("hidden");
+    showResults();
+  }
+  //AZZERARE IL TIMER E MOSTRARE LA NUOVA DOMANDA
+};
+
+const showResults = () => {
+  console.log("Siamo gia arrivati ai risultati");
+};
+
 const feedbackSection = document.querySelector("#fieldsetFeedback");
 const stelle = 10;
 for (let i = 0; i < stelle; i++) {
