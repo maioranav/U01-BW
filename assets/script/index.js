f16530d9f806f14db2a93b109b32ad918f7acf12
Antonio D'Amico <andam@hotmail.it>
2022-12-22 10:07:47 +0100

    timer created

--- assets/script/index.js
+++ assets/script/index.js
@@ -160,6 +160,61 @@ const loadQuestions = (questionsArray) => {
   nextQuestion();
 };
 
+// **************************************** TIMER *********************************************** //
+// Start with an initial value of 20 seconds
+const TIME_LIMIT = 30;
+
+// Initially, no time has passed, but this will count up
+// and subtract from the TIME_LIMIT
+let timePassed = 0;
+let timeLeft = TIME_LIMIT;
+
+function formatTimeLeft(time) {
+  // The largest round integer less than or equal to the result of time divided being by 60.
+  const minutes = Math.floor(time / 60);
+  // Seconds are the remainder of the time divided by 60 (modulus operator)
+  let seconds = time % 60;
+
+  // If the value of seconds is less than 10, then display seconds with a leading zero
+  if (seconds < 10) {
+    seconds = `0${seconds}`;
+  }
+
+  // The output in MM:SS format
+  return `${minutes}:${seconds}`;
+}
+
+let timerInterval = null;
+
+document.getElementById("app").innerHTML = `...`;
+
+function startTimer() {
+  timerInterval = setInterval(() => {
+    // The amount of time passed increments by one
+    timePassed = timePassed += 1;
+    timeLeft = TIME_LIMIT - timePassed;
+
+    // The time left label is updated
+    document.getElementById("base-timer-label").innerHTML =
+      formatTimeLeft(timeLeft);
+  }, 1000);
+}
+
+document.getElementById("app").innerHTML = `
+<div class="base-timer">
+  <svg class="base-timer__svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+    <g class="base-timer__circle">
+      <circle class="base-timer__path-elapsed" cx="50" cy="50" r="45" />
+      </g>
+      </svg>
+      <span id="base-timer-label" class="base-timer__label">
+      ${formatTimeLeft(timeLeft)}
+      </span>
+      </div>
+      `;
+
+// **************************************** FINE TIMER ***********************************************
+
 const nextQuestion = () => {
   switch (true) {
     case activeQuestion < 0:
@@ -168,7 +223,7 @@ const nextQuestion = () => {
       document
         .querySelector(`#domanda${activeQuestion}`)
         .classList.remove("hidden");
-      console.log(activeQuestion);
+      startTimer();
       break;
     case activeQuestion >= 0 && activeQuestion < questions.length - 1:
       document
